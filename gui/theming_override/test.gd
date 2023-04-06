c5459752a6b9bb04e0aa150b9da325ab45f27879
Relintai <relintai@protonmail.com>
2023-04-06 22:00:13 +0200

    Make sure everything works in the gui folder.

--- gui/theming_override/test.gd
+++ gui/theming_override/test.gd
@@ -10,7 +10,7 @@ onready var button = $VBoxContainer/Button
 onready var button2 = $VBoxContainer/Button2
 onready var reset_all_button = $VBoxContainer/ResetAllButton
 # Save the label color so it can be reset.
-onready var default_label_color = label.get_color("font_color")
+onready var default_label_color = label.get_theme_color("font_color")
 
 func _ready():
 	# Focus the first button automatically for keyboard/controller-friendly navigation.
@@ -22,33 +22,33 @@ func _on_button_pressed():
 	# to get a correct appearance when the button is hovered or pressed.
 	# We can't use a single StyleBox for all of them as these have different
 	# background colors.
-	var new_stylebox_normal = button.get_stylebox("normal").duplicate()
+	var new_stylebox_normal = button.get_theme_stylebox("normal").duplicate()
 	new_stylebox_normal.border_color = Color(1, 1, 0)
-	var new_stylebox_hover = button.get_stylebox("hover").duplicate()
+	var new_stylebox_hover = button.get_theme_stylebox("hover").duplicate()
 	new_stylebox_hover.border_color = Color(1, 1, 0)
-	var new_stylebox_pressed = button.get_stylebox("pressed").duplicate()
+	var new_stylebox_pressed = button.get_theme_stylebox("pressed").duplicate()
 	new_stylebox_pressed.border_color = Color(1, 1, 0)
 
-	button.add_stylebox_override("normal", new_stylebox_normal)
-	button.add_stylebox_override("hover", new_stylebox_hover)
-	button.add_stylebox_override("pressed", new_stylebox_pressed)
+	button.add_theme_stylebox_override("normal", new_stylebox_normal)
+	button.add_theme_stylebox_override("hover", new_stylebox_hover)
+	button.add_theme_stylebox_override("pressed", new_stylebox_pressed)
 
-	label.add_color_override("font_color", Color(1, 1, 0.5))
+	label.add_theme_color_override("font_color", Color(1, 1, 0.5))
 
 
 func _on_button2_pressed():
-	var new_stylebox_normal = button2.get_stylebox("normal").duplicate()
+	var new_stylebox_normal = button2.get_theme_stylebox("normal").duplicate()
 	new_stylebox_normal.border_color = Color(0, 1, 0.5)
-	var new_stylebox_hover = button2.get_stylebox("hover").duplicate()
+	var new_stylebox_hover = button2.get_theme_stylebox("hover").duplicate()
 	new_stylebox_hover.border_color = Color(0, 1, 0.5)
-	var new_stylebox_pressed = button2.get_stylebox("pressed").duplicate()
+	var new_stylebox_pressed = button2.get_theme_stylebox("pressed").duplicate()
 	new_stylebox_pressed.border_color = Color(0, 1, 0.5)
 
-	button2.add_stylebox_override("normal", new_stylebox_normal)
-	button2.add_stylebox_override("hover", new_stylebox_hover)
-	button2.add_stylebox_override("pressed", new_stylebox_pressed)
+	button2.add_theme_stylebox_override("normal", new_stylebox_normal)
+	button2.add_theme_stylebox_override("hover", new_stylebox_hover)
+	button2.add_theme_stylebox_override("pressed", new_stylebox_pressed)
 
-	label.add_color_override("font_color", Color(0.5, 1, 0.75))
+	label.add_theme_color_override("font_color", Color(0.5, 1, 0.75))
 
 
 func _on_reset_all_button_pressed():
@@ -56,12 +56,12 @@ func _on_reset_all_button_pressed():
 	# - `null` for fonts, icons, styleboxes, and shaders.
 	# - `0` for constants.
 	# - Colors must be reset manually by adding the previous color value as an override.
-	button.add_stylebox_override("normal", null)
-	button.add_stylebox_override("hover", null)
-	button.add_stylebox_override("pressed", null)
+	button.add_theme_stylebox_override("normal", null)
+	button.add_theme_stylebox_override("hover", null)
+	button.add_theme_stylebox_override("pressed", null)
 
-	button2.add_stylebox_override("normal", null)
-	button2.add_stylebox_override("hover", null)
-	button2.add_stylebox_override("pressed", null)
+	button2.add_theme_stylebox_override("normal", null)
+	button2.add_theme_stylebox_override("hover", null)
+	button2.add_theme_stylebox_override("pressed", null)
 
-	label.add_color_override("font_color", default_label_color)
+	label.add_theme_color_override("font_color", default_label_color)
